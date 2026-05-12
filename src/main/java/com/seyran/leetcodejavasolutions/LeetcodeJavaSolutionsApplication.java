8a3285855d8d84d9c305d0d5a5aab5865a0ae2a9
seyran7 <brsr888@gmail.com>
2026-05-12 22:45:16 +0400

    Initial LeetCode repository setup

---
+++ src/main/java/com/seyran/leetcodejavasolutions/LeetcodeJavaSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.seyran.leetcodejavasolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeJavaSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeJavaSolutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seyran/leetcodejavasolutions/LeetcodeJavaSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyran.leetcodejavasolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeJavaSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
