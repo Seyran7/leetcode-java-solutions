f64edaedd6a782ca90a684b8fab75af1aebc509c
seyran7 <brsr888@gmail.com>
2026-05-16 22:39:19 +0400

    solve: implement Palindrome Number without string conversion

---
+++ src/main/java/com/seyran/leetcodejavasolutions/easy/PalindromeSolution.java
@@ -0,0 +1,21 @@
+package com.seyran.leetcodejavasolutions.easy;
+
+public class PalindromeSolution {
+        public boolean isPalindrome(int x) {
+
+
+            if (x < 0) return false;
+
+            int original = x;
+            int reversed = 0;
+
+            while (x != 0) {
+
+                int digit = x % 10;
+                reversed = reversed * 10 + digit;
+                x = x / 10;
+            }
+
+            return original == reversed;
+    }
+}
