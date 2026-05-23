8fd1239caf7f7c1e45190ad4890b6ca1d09b809e
seyran7 <brsr888@gmail.com>
2026-05-23 22:26:27 +0400

    Solve Single Number using XOR (O(n) time, O(1) space)

---
+++ src/main/java/com/seyran/leetcodejavasolutions/easy/SingleNumber.java
@@ -0,0 +1,14 @@
+package com.seyran.leetcodejavasolutions.easy;
+
+public class SingleNumber {
+
+        public int singleNumber(int[] nums) {
+            int result = 0;
+
+            for (int num : nums) {
+                result ^= num;
+            }
+
+            return result;
+    }
+}
