7cb11a73783ab317dbf2c4a1e9c216a06e8f4432
seyran7 <brsr888@gmail.com>
2026-05-26 23:31:09 +0400

    Solve Power of Four using bit manipulation

---
+++ src/main/java/com/seyran/leetcodejavasolutions/easy/PowerOfFour.java
@@ -0,0 +1,14 @@
+package com.seyran.leetcodejavasolutions.easy;
+
+public class PowerOfFour {
+
+        public boolean isPowerOfFour(int n) {
+            if (n <= 0) return false;
+
+            while (n % 4 == 0) {
+                n /= 4;
+            }
+
+            return n == 1;
+    }
+}
