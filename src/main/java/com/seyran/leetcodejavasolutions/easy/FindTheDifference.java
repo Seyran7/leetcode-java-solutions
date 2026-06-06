f619733a97b9878415ae5f5972d9bbac3eaa7f29
seyran7 <brsr888@gmail.com>
2026-06-06 21:44:39 +0400

    Solve 'Find the Difference' using XOR technique

---
+++ src/main/java/com/seyran/leetcodejavasolutions/easy/FindTheDifference.java
@@ -0,0 +1,18 @@
+package com.seyran.leetcodejavasolutions.easy;
+
+public class FindTheDifference {
+
+        public char findTheDifference(String s, String t) {
+            int result = 0;
+
+            for (char c : s.toCharArray()) {
+                result ^= c;
+            }
+
+            for (char c : t.toCharArray()) {
+                result ^= c;
+            }
+
+            return (char) result;
+    }
+}
