542c9d93828f2c55b92b434197a04c38bee4f6b0
seyran7 <brsr888@gmail.com>
2026-06-11 17:26:07 +0400

    Solve LeetCode 27: Remove Element using two-pointer approach

---
+++ src/main/java/com/seyran/leetcodejavasolutions/easy/RemoveElement.java
@@ -0,0 +1,19 @@
+package com.seyran.leetcodejavasolutions.easy;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+
+        }
+
+        return k;
+    }
+}
