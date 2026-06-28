1dc4f164fcfb6268e4cd4f8816e1f7c4d6b1b1d9
seyran7 <brsr888@gmail.com>
2026-06-28 22:35:47 +0400

    Solve the leetcode problem

---
+++ src/main/java/com/seyran/leetcodejavasolutions/easy/PowerFour.java
@@ -0,0 +1,17 @@
+package com.seyran.leetcodejavasolutions.easy;
+
+public class PowerFour {
+
+    public class PowerOfFour {
+
+        public boolean isPowerOfFour(int n) {
+            if (n <= 0) return false;
+
+            while (n % 4 == 0) {
+                n /= 4;
+            }
+
+            return n == 1;
+        }
+    }
+}
